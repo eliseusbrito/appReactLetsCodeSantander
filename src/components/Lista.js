7db6527da95551b6c89d56765488e0d8cc3b3cc1
eliseusbrito <eliseusbrito@gmail.com>
2021-07-11 07:58:03 -0300

    Renderizado criando um componente que sera usado como item da lista

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -5,16 +5,42 @@ class Lista extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { items: ["item1", "item2", "item3", "item4"] }
+        this.state = {
+            items: [
+                { id: 1, nome: 'item1', completo: false },
+                { id: 2, nome: 'item2', completo: false },
+                { id: 3, nome: 'item3', completo: true },
+                { id: 4, nome: 'item4', completo: false },
+            ]
+        }
     }
 
     render() {
-        return(
-        <>
-            <ul>
-                {this.state.items.map((item, indice) => <li key={indice}>{item}</li>)}
-            </ul>
-        </>
+        return (
+            <>
+                <ul>
+                    {this.state.items.map((item) => (
+                        <Item key={item.id} id={item.id} completo={item.completo}>
+                            {item.nome}
+                        </Item>
+                    ))}
+                </ul>
+            </>
+        )
+    }
+}
+
+class Item extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const textDecoration = this.props.completo ? 'line-through' : 'none'
+        return (
+            <li data-id={this.props.id} style={{ textDecoration }}>
+                {this.props.children}
+            </li>
         )
     }
 }
